4097ec7ad6124b716daac5d9090eb3474f3fcc6d
dkruse <unconfigured@null.spigotmc.org>
2022-01-13 20:16:36 +0100

    WoZ added

--- reservation.js
+++ reservation.js
@@ -101,3 +101,56 @@ function reserve() {
     $('#main-box').html(result_html);
     return;
 }
+
+
+
+//WOZ
+var loginname = 'admin'
+var loginpass = 'admin'
+
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function() {
+    // console.log("onreadystatechange: " + request.readyState + ", " +  request.status);
+    // console.log(request.responseText);
+    if (request.readyState == 4) {
+        if (request.status == 200) {
+            var response = JSON.parse(request.responseText);
+            handlers[response._id](response);
+        }
+        if (request.status == 404) {
+            console.log("not found: " + request.responseText);
+        }
+    }
+};
+
+function get(variable) {
+    // console.log("get " + variable);
+    request.open("GET", dburl + variable, false);
+	request.setRequestHeader("Authorization", "Basic " + btoa(loginname + ":" + loginpass));
+    request.send();
+}
+
+function update() {
+    for (var name in handlers) {
+        // console.log("updating " + name);
+        get(name);
+    }
+}
+
+// request updates at a fixed interval (ms)
+var intervalID = setInterval(update, 1000);
+
+///////////////////////////////////////////////////////////////////////////////
+// your code below
+
+var dbname = "gmci";
+var dburl = "http://127.0.0.1:5984/" + dbname + "/";
+var handlers = {
+    "mapImage" : updateMap,
+};
+
+function updateMap(response) {
+    document.getElementById(response._id).src = response.src;
+    document.getElementById(response._id).width = response.width;
+}
